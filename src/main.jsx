a9e2f4aa62b9d44628cc03b71c55550ff9415e74
Arthur Amaral <arthuramaral6@gmail.com>
2023-10-07 16:13:15 -0300

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Inventory } from './pages/Inventory'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Inventory />
+  </React.StrictMode>
+)
