3695fb10c3c7cbde303ba3ca818f1bea2466bd5e
Arthur Amaral <arthuramaral6@gmail.com>
2023-10-30 21:13:41 -0300

    testing import.meta

--- src/pages/Inventory/index.jsx
+++ src/pages/Inventory/index.jsx
@@ -2,6 +2,8 @@ import { FilterableProductTable } from '../../components/FilterableProductTable'
 import './styles.css'
 
 export function Inventory() {
+  console.log(import.meta)
+  console.log(import.meta.url)
   return (
     <div className="container">
       <FilterableProductTable />
